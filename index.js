5eaa95f91936f1bb78fd7c61f2f055372a89730a
erlinggroestad <erling.groestad@gmail.com>
2021-12-07 11:24:54 +0100

    oppgave 1.3

---
+++ index.js
@@ -0,0 +1,4 @@
+/*Hentet herfra:https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript*/
+function byttfarge() {
+  document.body.style.backgroundColor = "#DDA0DD";
+}
